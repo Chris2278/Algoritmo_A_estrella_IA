1b8a78bf283932b9aee7e0028f5821e2cfdff0ba
Chris2278 <111544298+Chris2278@users.noreply.github.com>
2022-11-30 23:27:47 -0600

    Add files via upload

---
+++ A_Star_Search_Algorithm-master/App.py
@@ -0,0 +1,46 @@
+#Christian Alexis Paredes Arroyo   20110104  6E1
+#En esta practica se se utiliza el algoritmo A*
+
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+# App Main
+
+import sys
+from Node import Node
+from AStarAlgorithm import AStarAlgorithm
+
+class App(object):
+
+    def __init__(self):
+        print("Creando nodos: ", end = '')
+        self.i = Node("I", 7)
+        self.a = Node("A", 6)
+        self.b = Node("B", 2)
+        self.c = Node("C", 1)
+        self.f = Node("F", 0)
+        self.i.addChild(self.a, 1)
+        self.i.addChild(self.b, 4)
+        print("I ", end ='')
+        self.a.addChild(self.b, 2)
+        self.a.addChild(self.c, 5)
+        self.a.addChild(self.f, 12)
+        print("A ", end ='')
+        self.b.addChild(self.c, 2)
+        print("B ", end ='')
+        self.c.addChild(self.f, 3)
+        print("C ", end ='')
+        print("F ", end ='')
+
+    def searchAStarAlgorithm(self):
+        print("\nBuscando ruta optima con Algoritmo A*")
+        astar = AStarAlgorithm(self.i, self.f)
+        print("Nodo Inicial: %s -----> Nodo Final %s" % (self.i.name, self.f.name))
+        astar.run()
+
+
+if __name__ == "__main__":
+    try:
+        app = App()
+        app.searchAStarAlgorithm()
+    except (ValueError, FileNotFoundError, AttributeError) as ex:
+        print(ex)
